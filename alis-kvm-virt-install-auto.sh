d8d47a523a2851f8e0ebc10ce9283288c981529b
Dmitry Savosh <dysh.apps@gmail.com>
2023-07-23 17:33:44 +0300

    alis-config-efi-ext4-i3-wme-qemu

--- alis-kvm-virt-install-auto.sh
+++ alis-kvm-virt-install-auto.sh
@@ -87,7 +87,8 @@ done
 
 echo "Connecting..."
 
-./alis-cloud-init-ssh.sh -i $ip_address -c ""
+# ./alis-cloud-init-ssh.sh -i $ip_address -c ""
 # ./alis-cloud-init-ssh.sh -i $ip_address -c alis-config-efi-ext4-grub-gnome-qemu.sh
+./alis-cloud-init-ssh.sh -i $ip_address -c alis-config-efi-ext4-i3-wme-qemu.sh
 
 
