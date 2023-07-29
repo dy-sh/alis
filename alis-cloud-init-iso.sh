8464fe23e6078c5932bdf0b639c7aca372c108c8
Dmitry Savosh <dysh.apps@gmail.com>
2023-07-29 14:48:26 +0300

    mkisofs

--- alis-cloud-init-iso.sh
+++ alis-cloud-init-iso.sh
@@ -9,5 +9,6 @@ SSH_RSA=$(ssh-keygen -y -f cloud-init/alis.key)
 mkdir -p cloud-init/iso/
 cp cloud-init/meta-data cloud-init/user-data cloud-init/iso/
 sed -i "s#\\\${SSH_RSA}#${SSH_RSA}#" cloud-init/iso/user-data
-sudo pacman -S --needed --noconfirm cdrkit
+
+sudo pacman -S --needed --noconfirm cdrkit # mkisofs
 mkisofs -o cloud-init/alis-cloud-init.iso -V CIDATA -iso-level 3 -J -R cloud-init/iso/
