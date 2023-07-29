e44788e5ae1c417038c16e174eaa176fca60550f
Dmitry Savosh <dysh.apps@gmail.com>
2023-07-29 14:47:07 +0300

    cdrkit added

--- alis-cloud-init-iso.sh
+++ alis-cloud-init-iso.sh
@@ -9,5 +9,5 @@ SSH_RSA=$(ssh-keygen -y -f cloud-init/alis.key)
 mkdir -p cloud-init/iso/
 cp cloud-init/meta-data cloud-init/user-data cloud-init/iso/
 sed -i "s#\\\${SSH_RSA}#${SSH_RSA}#" cloud-init/iso/user-data
-# sudo pacman -S cdrkit
+sudo pacman -S --needed --noconfirm cdrkit
 mkisofs -o cloud-init/alis-cloud-init.iso -V CIDATA -iso-level 3 -J -R cloud-init/iso/
